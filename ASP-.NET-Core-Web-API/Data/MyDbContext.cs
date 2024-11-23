35f14bc4a22266c8138a7e3c0d2475366567b7a1
khanhdoan-2809 <khanhdoan21.work@gmail.com>
2024-11-23 13:09:24 +0700

    push

---
+++ ASP-.NET-Core-Web-API/Data/MyDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ASP_.NET_Core_Web_API.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions dbContextOptions): base(dbContextOptions) { }
+        
+        // public DbSet<bla> bla { get; set; }
+    
+    }
+}
--- ASP-.NET-Core-Web-API/Program.cs
+++ ASP-.NET-Core-Web-API/Program.cs
@@ -1,3 +1,6 @@
+using ASP_.NET_Core_Web_API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MyDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("MyConnectionString")))
 
 var app = builder.Build();
 
